51e3050de0506c55daee7f82cb279e5467860d78
jakabus <jakabus@gmail.com>
2018-08-27 11:41:33 +0200

    ezzelbaszakodok

---
+++ Counter/Counter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Give me a number: ");
+            n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Recursion(n));
+            Console.ReadKey();
+        }
+
+        public static int Recursion(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return Recursion(n - 1);
+            }
+        }
+    }
+}
